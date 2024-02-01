ac52238b68ff08fb246aa09e52892b6c0a16bc61
Ferian Satria <148834456+ferian-satria@users.noreply.github.com>
2024-02-01 22:36:20 +0700

    Control Flow
    Learn about Switch Case

---
+++ switch_case.dart
@@ -0,0 +1,26 @@
+void main() {
+  final firstNumber = 12;
+  final secondNumber = 13;
+  final operator = "+";
+
+  switch (operator) {
+    case '+':
+      print(
+        '$firstNumber $operator $secondNumber = ${firstNumber + secondNumber}');
+      break;
+    case '-':
+      print(
+          '$firstNumber $operator $secondNumber = ${firstNumber - secondNumber}');
+      break;
+    case '*':
+      print(
+          '$firstNumber $operator $secondNumber =${firstNumber * secondNumber}');
+      break;
+    case '/':
+      print(
+          '$firstNumber $operator $secondNumber = ${firstNumber / secondNumber}');
+      break;
+    default:
+      print('Operator tidak ditemukan');
+  }
+}
